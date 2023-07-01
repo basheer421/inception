f3cacd963c8237b77695e876440379873db9b7f2
basheer421 <basheer123b2@gmail.com>
2023-07-01 13:14:37 -0500

    db working

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-mysql_install_db --user=mysql --datadir=/var/lib/mysql
-echo "[mysqld]" > /etc/mysql/my.cnf
-echo "bind-address = 0.0.0.0" >> /etc/mysql/my.cnf
-rc-service mariadb starts
-rc-update add mariadb default
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
   mysql -uroot  -e \
   "CREATE DATABASE $DB_NAME;"
