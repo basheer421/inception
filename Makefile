a456abcb4c16a96f11253928d6d6e2f0116299e5
basheer421 <basheer123b2@gmail.com>
2023-07-01 18:35:21 -0500

    hello world

--- Makefile
+++ Makefile
@@ -1,11 +1,21 @@
+all: up
+
+build:
+	docker compose -f srcs/docker-compose.yml up -d --build
 
 up:
 	mkdir -p /home/${USER}/data/mariadb
 	mkdir -p /home/${USER}/data/wordpress
 	docker compose -f srcs/docker-compose.yml up -d
 
+down:
+	docker compose -f srcs/docker-compose.yml down
+
 vclean:
+	$(MAKE) down
 	docker system prune -a
 
 re: vclean
 	$(MAKE) up
+
+.PHONY	: all build down re clean fclean
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-# https://codingwithmanny.medium.com/custom-wordpress-docker-setup-8851e98e6b8
-mkdir -p /usr/share/webapps/
 
-cd /usr/share/webapps/
+mkdir -p /var/www/html/
+cd /var/www/html/
 wget http://wordpress.org/latest.tar.gz
-tar -xzvf latest.tar.gz
+tar -xzf latest.tar.gz
 rm latest.tar.gz
-chown -R nginx /usr/share/webapps/
-ln -s /usr/share/webapps/wordpress/ /var/www/localhost/htdocs/wordpress
+cd wordpress
+mv wp-config-sample.php wp-config.php
+sed -i "s/'Your database name'/'$DB_NAME'/g" wp-config.php
+sed -i "s/'The created username'/'$DB_USER'/g" wp-config.php
+sed -i "s/'The password you set'/'$DB_PASS'/g" wp-config.php
+rm index.php
+echo "hello world" > index.html
