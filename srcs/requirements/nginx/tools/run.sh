8bf66d780bc29584d3364f33178cc83ea86837c4
basheer421 <basheer123b2@gmail.com>
2023-07-09 18:10:29 -0500

    added ssl

--- srcs/requirements/nginx/tools/run.sh
+++ srcs/requirements/nginx/tools/run.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+openssl \
+    req -x509 \
+    -nodes \
+    -subj "/CN=${DOMAIN_NAME}}" \
+    -addext "subjectAltName=DNS:${DOMAIN_NAME}" \
+    -days 365 \
+    -newkey rsa:2048 -keyout /etc/self-signed.key \
+    -out /etc/self-signed.crt
 
 mkdir -p /run/nginx;
 nginx -g 'daemon off;'
